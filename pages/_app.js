98296dedf3adfa90b2305e2a045d4a7683967e30
sanatankyon <sanatankyon@gmail.com>
2026-09-15 07:24:45 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,24 @@
+import Script from 'next/script'
 import Header from '../components/Header'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Script id="google-translate-init" strategy="afterInteractive">
+        {`
+          function googleTranslateElementInit() {
+            new google.translate.TranslateElement(
+              { pageLanguage: 'en', autoDisplay: false },
+              'google_translate_element'
+            );
+          }
+        `}
+      </Script>
+      <Script
+        src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+        strategy="afterInteractive"
+      />
       <Header />
       <Component {...pageProps} />
     </>
